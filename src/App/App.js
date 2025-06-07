4e5f32b43ce56d912338ff3a8ae1a7c3ee03fd7f
Igor Tamuz <igortamuz@gmail.com>
2025-06-06 21:49:31 -0300

    Troca de classNames para ingles, adicionado scroll no inicio e troca de css no termos

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../Components/Header/header";
 import "../ResetCss/reset.css";
 import Footer from "../Components/Footer/footer";
-import Home from "../Components/Bodys/Home/home";
-import Termos from "../Components/Bodys/Termos/termos";
+import Home from "../Components/Bodys/home/home";
+import Termos from "../Components/Bodys/termos/termos";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
+			<Route path="/Entrada" element={<Home />} />
             <Route path="/Termos" element={<Termos />} />
           </Routes>
         </main>
