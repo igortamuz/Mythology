622cb3b6da27954725beb604dd6afec5cba12c01
Igor Tamuz <igortamuz@gmail.com>
2025-06-08 05:44:16 -0300

    Adicao de diversas melhorias: Responsividade, organizacao de padrao do CSS, correcao de calculo, e adicao de identificador de palavras para buffs, debuffs e efeitos

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "../Pages/Header/header";
-import "../ResetCss/reset.css";
-import Footer from "../Pages/Footer/footer";
-import Home from "../Pages/Bodys/home/home";
-import Terms from "../Pages/Bodys/terms/terms";
-import GodsPage from "../Pages/Bodys/gods/godsPage";
+import Header from "../Components/Header/header";
+import "../GlobalCss/reset.css";
+import Footer from "../Components/Footer/footer";
+import Home from "../Components/Bodys/home/home";
+import Terms from "../Components/Bodys/terms/terms";
+import GodsPage from "../Components/Bodys/gods/godsPage";
+import GlobalKeywordApplier from "../GlobalWrapper/globalApplier"; 
 
 function App() {
   return (
@@ -12,12 +13,14 @@ function App() {
       <div className="app-container">
         <Header />
         <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-			<Route path="/Entrada" element={<Home />} />
-            <Route path="/Termos" element={<Terms />} />
-            <Route path="/panteao/:panteao/:nomeDeus" element={<GodsPage />} />
-          </Routes>
+          <GlobalKeywordApplier>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Entrada" element={<Home />} />
+              <Route path="/Termos" element={<Terms />} />
+              <Route path="/panteao/:panteao/:nomeDeus" element={<GodsPage />} />
+            </Routes>
+          </GlobalKeywordApplier>
         </main>
         <Footer />
       </div>
